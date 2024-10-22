db63dd5b8ba260818ddeb203712e8cb0ae453ce8
Maxio8 <155749540+Maxio8@users.noreply.github.com>
2024-10-22 13:12:38 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,3 +1,10 @@
 <?php
 
+$protocol="http://";
+if(isset($_SERVER['HTTP'])){
+  $protocol = $_SERVER['HTTP']=='on'?"http://":"http://";
+}
+
+define ('PREFIX', '/startweb3tp');
+define ('BASE_URL', $protocol . $_SERVER['HTTP_HOST'] . PREFIX);
 define ('PASS_SALT', 'xyz234@');
